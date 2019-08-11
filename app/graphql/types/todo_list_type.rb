e30eea366c2863bbac38fc106525732adbfb4e05
Pablo Levy <pablo@busolinea.com>
2019-08-11 13:51:33 -0500

    add graphql types

---
+++ app/graphql/types/todo_list_type.rb
@@ -0,0 +1,9 @@
+module Types
+  TodoListType = GraphQL::ObjectType.define do
+    name 'TodoListType'
+    description 'The Todo List type'
+
+    field :id, !types.ID
+    field :title, !types.String
+  end
+end
