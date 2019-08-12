cc68854b24f3cc6495a642997b37af47def632d9
Pablo Levy <pablo@busolinea.com>
2019-08-11 21:31:07 -0500

    use uuid

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  after_initialize :set_uuid, if: :new_record?
+
+  def set_uuid
+    self.id ||= SecureRandom.uuid
+  end
 end
--- db/migrate/20190811174007_create_todo_lists.rb
+++ db/migrate/20190811174007_create_todo_lists.rb
@@ -1,9 +1,11 @@
 class CreateTodoLists < ActiveRecord::Migration[5.2]
   def change
-    create_table :todo_lists do |t|
+    create_table :todo_lists, id: false do |t|
+      t.string :id, primary_key: true, null: false
       t.string :title
 
-      t.timestamps
+      t.datetime :created_at, limit: 6
+      t.datetime :updated_at, limit: 6
     end
   end
 end
